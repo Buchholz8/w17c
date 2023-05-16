6de27d7dfa8d07e7995afdcada675dd6be3c8e1e
markb <markbuchholz8@gmail.com>
2023-05-15 22:46:35 -0600

    fixed blog.sql should have checks

--- queries.sql
+++ queries.sql
@@ -1,13 +1,59 @@
-insert into blogger(username, password, bio, name, created_at, email, age) values('user1' , 'password' , 'this is user 1' , 'john' , '2021-08-15' , 'user1@new.com' , '20')
-insert into blogger(username, password, bio, name, created_at, email, age) values('user2' , 'pass' , 'this is user 2' , 'frank' , '2022-12-10' , 'user2@new.com' , '25')
-insert into blogger(username, password, bio, name, created_at, email, age) values('user3' , 'password' , 'this is user 3' , 'sarah' , '2023-10-22' , 'user3@new.com' , '30')
-insert into blogger(username, password, bio, name, created_at, email, age) values('user4' , 'password' , 'this is user 4' , 'craig' , '2020-01-24' , 'test@test.com' , '10')
-insert into blogger(username, password, bio, name, created_at, email, age) values('user5' , 'password' , 'this is user 5' , 'jerry' , '2018-06-07' , 'user5@new.com' , '15')
-insert into blogger(username, password, bio, name, created_at, email, age) values('user6' , 'password' , 'this is user 6' , 'hannah' , '2023-05-15' , 'user6@new.com' , '35')
-insert into blogger(username, password, bio, name, created_at, email, age) values('user7' , 'password' , 'this is user 7' , 'leroy' , '2023-03-14' , 'user7@new.com' , '60')
-insert into blogger(username, password, bio, name, created_at, email, age) values('user8' , 'password' , 'this is user 8' , 'terry' , '2020-02-19' , 'user8@new.com' , '45')
-insert into blogger(username, password, bio, name, created_at, email, age) values('user9' , 'password' , 'this is user 9' , 'lizzy' , '2021-11-02' , 'user9@new.com' , '15')
-insert into blogger(username, password, bio, name, created_at, email, age) values('test' , 'password' , 'this is user 10' , 'adam' , '2019-09-01' , 'user10@new.com' , '20')
-select from blogger where created_at >= 2020-01-01
-delete from blogger where username = 'test'
-update blogger set password = 'bad_password' where password = 'pass'
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: blog_posts
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.6.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `blogger`
+--
+
+DROP TABLE IF EXISTS `blogger`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `blogger` (
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `bio` varchar(150) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `age` decimal(10,0) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `blogger_un` (`username`,`email`),
+  CONSTRAINT `blogger_check` CHECK (`age` > 0 and `age` < 120)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blogger`
+--
+
+LOCK TABLES `blogger` WRITE;
+/*!40000 ALTER TABLE `blogger` DISABLE KEYS */;
+INSERT INTO `blogger` VALUES ('user1','password','this is user 1','user','2023-05-15','user1@new.com',20,1),('user2','bad_password','this is user 2','frank','2022-12-10','user2@new.com',25,2),('user3','password','this is user 3','sarah','2023-10-22','user3@new.com',30,4),('user4','password','this is user 4','craig','2020-01-24','test@test.com',10,5),('user5','password','this is user 5','jerry','2018-06-07','user5@new.com',15,6),('user6','password','this is user 6','hannah','2023-05-15','user6@new.com',35,7),('user7','password','this is user 7','leroy','2023-03-14','user7@new.com',60,8),('user8','password','this is user 8','terry','2020-02-19','user8@new.com',45,9),('user9','password','this is user 9','lizzy','2021-11-02','user9@new.com',15,10);
+/*!40000 ALTER TABLE `blogger` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-15 22:45:58
